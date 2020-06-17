0c4983d109d28383acbda2299705a8c10d9c90e2
smukV <vasya.smuk@mail.ru>
2020-06-17 16:47:28 +0300

    finished string

--- sv_alast/src/svString.cpp
+++ sv_alast/src/svString.cpp
@@ -3,35 +3,87 @@
 namespace sv_alast{
 	String::String(const char * str) 
 	{
-		size = strlen(str);
-		data = new char[size + 1];
-		strcpy(data, str);
+		m_Size = StrLen(str);
+		m_Data = new char[m_Size+ 1];
+		StrCpy(m_Data, str);
 	}
-	String::String(const String & other)
+	String::String(const String& other)
 	{
+		m_Size = other.m_Size;
+		m_Data = new char[m_Size + 1];
+		StrCpy(m_Data, other.m_Data);
 	}
-	String & String::operator=(const String & other)
+	String::String(String && other) noexcept
 	{
-		// TODO: insert return statement here
+		m_Size = other.m_Size;
+		m_Data = other.m_Data;
+
+		other.m_Size = 0;
+		other.m_Data = nullptr;
 	}
-	size_t strlen(const char * str)
+	String& String::operator=(const String & other)
+	{
+		if (m_Data != nullptr)
+			delete m_Data;
+		m_Size = other.m_Size;
+		m_Data = new char[m_Size + 1];
+		StrCpy(m_Data, other.m_Data);
+		return *this;
+	}
+
+	String String::operator+(const String & other) const
+	{
+		size_t res_Size = m_Size + other.m_Size;
+		char* res_Data = new char[res_Size + 1];
+
+		StrCpy(res_Data, this->m_Data);
+		StrCpy(res_Data, other.m_Data, m_Size, other.m_Size);
+
+		return String(res_Data);
+	}
+
+	String& String::operator+=(const String & other)
+	{
+		(*this) = (*this) + other;
+		return *this;
+	}
+
+	const char * String::Data() const
+	{
+		return m_Data;
+	}
+
+	const size_t String::Size() const
+	{
+		return m_Size;
+	}
+	
+	size_t StrLen(const char * str)
 	{
 		size_t result = 0;
 		for (result; str[result] != '\0'; result++);
 		return result;
 	}
-	void strcpy(char * destStr, const char * srcStr)
+	void StrCpy(char * destStr, const char * srcStr)
 	{
 		size_t i = 0;
 		for (i; srcStr[i] != '\0'; i++)
 			destStr[i] = srcStr[i];
 		destStr[i] = '\0';
 	}
-	void strcpy(char * destStr, const char * srcStr, size_t size)
+	void StrCpy(char * destStr, const char * srcStr, size_t size)
 	{
 		size_t i;
 		for(i = 0; i <= size; i++)
 			destStr[i] = srcStr[i];
 		destStr[i] = '\0';
+	}
+	void StrCpy(char * destStr, const char * srcStr, size_t start_Pos, size_t size)
+	{
+		size_t i;
+		int end_Pos = start_Pos + size;
+		for (i = start_Pos; i <= end_Pos; i++)
+			destStr[i] = srcStr[i - start_Pos];
+		destStr[i] = '\0'; 
 	}
 }
--- sv_alast/src/svString.h
+++ sv_alast/src/svString.h
@@ -3,16 +3,22 @@ namespace sv_alast {
 	class String
 	{
 	private:
-		char* data;
-		size_t size;
+		char* m_Data;
+		size_t m_Size;
 	public:
-		String(): data(nullptr), size(0){}
+		String(): m_Data(nullptr), m_Size(0){}
 		String(const char* str);
 		String(const String& other);
+		String(String&& other) noexcept;
 		String& operator=(const String& other);
+		String operator+(const String& other) const;
+		String& operator+=(const String& other);
+		const char* Data()const;
+		const size_t Size() const;
 	};
 
-	size_t strlen(const char* str);
-	void strcpy(char* destStr, const char* srcStr);
-	void strcpy(const char* inStr, const char* outStr, size_t size);
+	size_t StrLen(const char* str);
+	void StrCpy(char* destStr, const char* srcStr);
+	void StrCpy(char* destStr, const char* srcStr, size_t size);
+	void StrCpy(char* destStr, const char* srcStr, size_t start_Pos, size_t size);
 }
